333e09e2b3e3c939d12929303b78d1d6693c9d6e
Benjamin Lee <bnllee@ucdavis.edu>
2020-02-23 11:18:13 -0800

    Adds css and add stringy endpoint

--- app.js
+++ app.js
@@ -9,29 +9,80 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const mapProvinceToCoord = {};
 // MONGO
+// const rawdataSchema = new mongoose.Schema({
+//   'Province/State': String,
+//   lat: String,
+//   lng: String,
+//   'Country/Region': String,
+//   'Last Update': String,
+//   Confirmed: String,
+//   Deaths: String,
+//   Recovered: String,
+// });
+// const Rawdata = mongoose.model('rawdata', rawdataSchema, 'rawdata');
+
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://EpicN:wGLI0ccfbrU9ngfv@cluster0-8iybi.gcp.mongodb.net/test?retryWrites=true&w=majority";
+// const uri = "mongodb+srv://EpicN:wGLI0ccfbrU9ngfv@cluster0-8iybi.gcp.mongodb.net/test";
+// const uri = "mongodb+srv://EpicN:<password>@cluster0-8iybi.gcp.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// console.log(client);
 client.connect(err => {
-  const collecion = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
+  if (err) {
+    console.log(err);
+  } else {
+    const collection = client.db("viralwatch").collection("rd2");
+
+
+    collection.find({}).toArray(function (err, docs) {
+      if (err) {
+        return console.log(err);
+      }
+
+      docs.forEach((doc) => {
+        const province = doc['Province/State'];
+        const state = doc['Country/Region'];
+        const lat = doc.lat;
+        const lng = doc.lng;
+        mapProvinceToCoord[`${province}, ${state}`] = { 'lat': lat, 'lng': lng };
+      });
+
+      console.log(docs);
+      delete mapProvinceToCoord[', null'];
+      console.log(mapProvinceToCoord);
+    });
+
+    // console.log(collection);
+
+    // perform actions on the collection object
+    /*collection.find({}, (err, documents) => {
+      // console.log('this ran', documents);
+      console.log(documents.toArray());
+      if (err) {
+        console.log('something went wrong with parsing data!');
+      } else {
+        documents.map(document => {
+          const province = document['Province/State'];
+          const state = document['Country/Region'];
+          const lat = document.lat;
+          const lng = document.lng;
+          mapProvinceToCoord[`${province}, ${state}`] = { 'lat': lat, 'lng': lng };
+          console.log(province);
+          client.close();
+        });
+        console.log('mapProvinceToCoord =', mapProvinceToCoord);
+      }
+    });*/
+  }
 });
 
-const countrySchema = new mongoose.Schema({
-  'Province/State': String,
-  lat: Number,
-  lng: Number,
-  'Country/Region': String,
-  'Last Update': Number,
-  Confirmed: Number,
-  Deaths: Number,
-  Recovered: Number,
-});
+
+
 
 
 // app.use(helmet.frameguard({
@@ -49,39 +100,93 @@ app.get('/', (req, res) => {
 });
 
 app.get('/find', (req, res) => {
-  res.render('find');
+  console.log('mapProvinceToCoord = ', mapProvinceToCoord);
+  res.render('find', { mapProvinceToCoord });
 });
 
+function replaceStringsWithNumbers(map) {
+  let newMap = map;
+  // for (key in Object.keys(map)) {
+  //   console.log(key);
+  //   newMap[key] = parseFloat(map[key]);
+  // }
+
+  Object.keys(map).forEach((key) => {
+    newMap[key] = parseFloat(map[key]);
+  });
+  return newMap;
+}
+
 app.post('/find', (req, res) => {
-  const { start, destination } = req.body;
+  let { start, waypointA, waypointB, destination } = req.body;
+  start = JSON.parse(start);
+  waypointA = JSON.parse(waypointA);
+  waypointB = JSON.parse(waypointB);
+  destination = JSON.parse(destination);
+
+  if (typeof start.lat === 'string') {
+    start = replaceStringsWithNumbers(start);
+    waypointA = replaceStringsWithNumbers(waypointA);
+    waypointB = replaceStringsWithNumbers(waypointB);
+    destination = replaceStringsWithNumbers(destination);
+  }
+
+  // console.log(req.body);
   // console.log(start);
   // console.log(destination);
 
-  const api_key = 'AIzaSyC3ABfQlr8Nj-T8xLSLcWePhYzA982e87k';
-  const uri_start = `https://maps.googleapis.com/maps/api/geocode/json?address=${start}&key=${api_key}`;
-  const uri_end = `https://maps.googleapis.com/maps/api/geocode/json?address=${destination}&key=${api_key}`;
+  // const api_key = 'AIzaSyC3ABfQlr8Nj-T8xLSLcWePhYzA982e87k';
+  // const uri_start = `https://maps.googleapis.com/maps/api/geocode/json?address=${start}&key=${api_key}`;
+  // const uri_waypoint_a = `https://maps.googleapis.com/maps/api/geocode/json?address=${waypointA}&key=${api_key}`;
+  // const uri_waypoint_b = `https://maps.googleapis.com/maps/api/geocode/json?address=${waypointB}&key=${api_key}`;
+  // const uri_end = `https://maps.googleapis.com/maps/api/geocode/json?address=${destination}&key=${api_key}`;
 
-  let startCoord, destinationCoord;
-  const fetchStart = fetch(uri_start).then((response) => {
-    return response.json();
-  }).then((json) => {
-    console.log(json);
-    startCoord = json.results[0].geometry.location;
-  });
+  // let startCoord, destinationCoord, waypointACoord, waypointBCoord;
+  // const fetchStart = fetch(uri_start).then((response) => {
+  //   return response.json();
+  // }).then((json) => {
+  //   console.log(json);
+  //   startCoord = json.results[0].geometry.location;
+  // });
 
+  // const fetchA = fetch(uri_waypoint_a).then((response) => {
+  //   return response.json();
+  // }).then((json) => {
+  //   console.log(json);
+  //   waypointACoord = json.results[0].geometry.location;
+  // });
 
-  const fetchDestination = fetch(uri_end).then((response) => {
-    return response.json();
-  }).then((json) => {
-    console.log(json);
-    destinationCoord = json.results[0].geometry.location;
-  });
+  // const fetchB = fetch(uri_waypoint_b).then((response) => {
+  //   return response.json();
+  // }).then((json) => {
+  //   console.log(json);
+  //   waypointBCoord = json.results[0].geometry.location;
+  // });
 
-  const redirectPromise = Promise.all([fetchStart, fetchDestination]).then(() => {
-    const path = [startCoord, destinationCoord];
-    res.redirect(`/show?path=${JSON.stringify(path)}`);
-  });
+  // const fetchDestination = fetch(uri_end).then((response) => {
+  //   return response.json();
+  // }).then((json) => {
+  //   console.log(json);
+  //   destinationCoord = json.results[0].geometry.location;
+  // });
+
+  // const redirectPromise = Promise.all([fetchStart, fetchDestination]).then(() => {
+  //   const path = [startCoord, destinationCoord];
+  //   const { startLat, startLong } = startCoord;
+  //   const { destLat, destLong } = destinationCoord;
+  //   console.log(startCoord);
+  //   console.log(waypointACoord);
+  //   console.log(waypointBCoord);
+  //   console.log(destinationCoord);
+  //   res.redirect(`/show?path=${JSON.stringify(path)}`);
+  // });
   // res.redirect('/show');
+  path = [start, waypointA, waypointB, destination];
+  // console.log('path.toString()', path.toString());
+  // console.log('JSON.stringify(path)', JSON.stringify(path));
+  console.log(`rendering...:/show?path=${JSON.stringify(path)}`)
+  res.redirect(`/show?path=${JSON.stringify(path)}`);
+  // res.redirect(`/ show ? path = [${ path.toString() }]`);
 });
 
 app.get('/show', (req, res) => {
